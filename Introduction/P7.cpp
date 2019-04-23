106e4a242d9a272661c6cc9a56dd0901c33c82d2
Kirtish Dhande <aniket.dhande@gmail.com>
2019-04-23 17:04:19 +0530

    Create P7.cpp

---
+++ Introduction/P7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int len;
+    cin>>len;
+    int arr[len];
+    for(int i=0;i<len;i++)
+        cin>>arr[i];
+
+    for(int i=len-1;i>=0;i--)
+        cout<<arr[i]<<" ";
+    return 0;
+}
